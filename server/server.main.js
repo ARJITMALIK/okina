2ed3a98be83d99967aca011056be113c55aba7bf
ARJITMALIK <arjitmalik2001@gmail.com>
2023-07-18 00:08:23 +0530

    added basic backend

---
+++ server/server.main.js
@@ -0,0 +1,20 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+const PORT = process.env.PORt || 5000;
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server successfully started at port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  console.log("Endpoint hitted");
+  res.send("yo ho ho ho ho");
+});
