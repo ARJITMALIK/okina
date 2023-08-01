640d529291e404232b85b42f662528d0683d674a
ARJITMALIK <arjitmalik2001@gmail.com>
2023-08-01 21:02:04 +0530

    quick fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,10 @@
-import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  @ViewChild('cnvs', { static: true }) mainCnvs!: ElementRef;
+export class AppComponent {
   title = 'okina';
-
-  public CANVAS_WIDTH: number = 600;
-  public CANVAS_HEIGHT: number = 600;
-
-  public frameX: number = 0;
-  public frameY: number = 0;
-
-  public spriteWidth: number = 300;
-  public spriteHeight: number = 300;
-
-  public gameFrame: number = 0;
-  public staggerFrame: number = 5;
-
-  public playerImg: HTMLImageElement = new Image();
-
-  public ctx!: CanvasRenderingContext2D;
-  ngOnInit(): void {
-    const canvas = this.mainCnvs.nativeElement;
-    canvas.width = this.CANVAS_WIDTH;
-    canvas.height = this.CANVAS_HEIGHT;
-    this.ctx = canvas.getContext('2d');
-    this.playerImg.src = '../assets/spritesheets/shinobi_spritesheet.png';
-    this.playerImg.onload = () => {
-      this.animate();
-    };
-  }
-
-  animate() {
-    this.ctx.clearRect(0, 0, this.CANVAS_WIDTH, this.CANVAS_HEIGHT);
-    this.ctx.drawImage(
-      this.playerImg,
-      this.frameX * this.spriteWidth,
-      this.frameY * this.spriteHeight,
-      this.spriteWidth,
-      this.spriteHeight,
-      100,
-      200,
-      this.spriteWidth,
-      this.spriteHeight
-    );
-    if (this.gameFrame % this.staggerFrame === 0) {
-      if (this.frameX < 3) {
-        this.frameX++;
-      } else {
-        this.frameX = 0;
-        this.gameFrame = 0;
-      }
-    }
-    this.gameFrame++;
-    requestAnimationFrame(this.animate.bind(this));
-  }
 }
